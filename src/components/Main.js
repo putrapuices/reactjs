37bf4674adaf2a7163ffc31f75d9cab8c7a9052f
putrapuices <puices14@gmail.com>
2021-11-16 22:09:34 +0700

    penulisan setState

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,31 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
-class Main extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            title:"Menu Makanan"
-        };
-        this.rubahaData = this.rubahaData.bind(this);
-    }
-rubahaData(){
-    this.setState({title:"Pilih Makanan"});
-}
-    render(){
-        return(
-            <div>
-                <h3>{this.state.title}</h3>
-                <button onClick={this.rubahaData}>Rubah Data</button>
-            </div>
-        );
-    }
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Menu Makanan",
+    };
+    this.rubahData = this.rubahData.bind(this);
+  }
+  //   cara pertama
+  //   rubahData() {
+  //     this.setState({ title: "Pilih Makanan" });
+  //   }
+  //cara kedua ini lebih disarankan
+  rubahData() {
+    this.setState((state, props) => {
+      return { title: "Pilih Menu Makanan" };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.state.title}</h3>
+        <button onClick={this.rubahData}>Rubah Data</button>
+      </div>
+    );
+  }
 }
 
 export default Main;
