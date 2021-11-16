5b780c65e06f028bc310a3a4c39c68c0d29e0fd5
putrapuices <puices14@gmail.com>
2021-11-16 22:14:17 +0700

    membuat multiple state

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ class Main extends Component {
     super(props);
     this.state = {
       title: "Menu Makanan",
+      title2: "Menu Minuman",
     };
     this.rubahData = this.rubahData.bind(this);
   }
@@ -15,13 +16,15 @@ class Main extends Component {
   //cara kedua ini lebih disarankan
   rubahData() {
     this.setState((state, props) => {
-      return { title: "Pilih Menu Makanan" };
+      //   return { title: "Pilih Menu Makanan", title2: "Pilih Menu Minuman" };
+      return { title: state.title2, title2: state.title };
     });
   }
   render() {
     return (
       <div>
         <h3>{this.state.title}</h3>
+        <h2>{this.state.title2}</h2>
         <button onClick={this.rubahData}>Rubah Data</button>
       </div>
     );
